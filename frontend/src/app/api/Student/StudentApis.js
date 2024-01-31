b22a7096bd57e8a1edfb405a550d377a58466ade
Vivek0624 <vivek.gundu29@gmail.com>
2024-01-31 21:03:43 +0530

    added UI student

---
+++ frontend/src/app/api/Student/StudentApis.js
@@ -0,0 +1,80 @@
+import { isAuthenticated } from "../../helpers/AuthHelpers";
+import { redirectOnTokenExpire } from "../Auth";
+const ENDPOINT = import.meta.env.VITE_BACKEND_ENDPOINT;
+
+export async function getStudentsViewApi() {
+  const { accessToken } = isAuthenticated();
+
+  const response = await fetch(`${ENDPOINT}/getStudentsView`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+
+  if (response.status == 401) {
+    redirectOnTokenExpire();
+  }
+
+  return response.json();
+}
+
+export async function getEmailsofpendingstudentsApi() {
+  const { accessToken } = isAuthenticated();
+
+  const response = await fetch(`${ENDPOINT}/getEmailsofpendingstudents`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+
+  if (response.status == 401) {
+    redirectOnTokenExpire();
+  }
+
+  return response.json();
+}
+
+export async function sendEmailToStudentMicrositeApi(data) {
+  const { accessToken } = isAuthenticated();
+
+  const response = await fetch(`${ENDPOINT}/sendemailtostudentmicrosite`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: accessToken,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.status == 401) {
+    redirectOnTokenExpire();
+  }
+
+  return response.json();
+}
+
+export async function executeScholarShipApplicationApi(data) {
+  const { accessToken } = isAuthenticated();
+
+  const response = await fetch(`${ENDPOINT}/executejar`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+
+  if (response.status == 401) {
+    redirectOnTokenExpire();
+  }
+
+  return response.json();
+}
